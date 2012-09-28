2a6b91b870246c6f09859dc9bd6bf137495cf71a
Dominic Fandrey <dominic.fandrey@highspeed-karlsruhe.de>
2012-09-28 18:14:53 +0200

    Only generate assembler sources for libraries with inline asm
    changed scripts/xml.awk
    - Steal updates from hsk_libs, adds some syntax sugar to generate output
      like ?Vision
    
    changed uVision/hsk_dev.uvproj
    - Change compiler defaults
    - Update overlay format
    - Fix mainpage.txt entry
    
    changed uVisionupdate.sh
    - Add compiler config for libraries with inline assembler

--- uVisionupdate.sh
+++ uVisionupdate.sh
@@ -28,8 +28,53 @@ for lib in $(
 -selectInserted
 -insert:FileName=${lib##*/}
 -insert:FileType=1
--insert:FilePath=../$lib
--select:.."
+-insert:FilePath=../$lib"
+	if grep -Eq '^[[:space:]]*#[[:space:]]*pragma[[:space:]](.*[[:space:]])?asm($|[[:space:]].*)' "$lib"; then
+		libdeps="$libdeps
+-insert:FileOption
+-selectInserted
+-insert:CommonProperty
+-selectInserted
+-insert:UseCPPCompiler=2
+-insert:RVCTCodeConst=0
+-insert:RVCTZI=0
+-insert:RVCTOtherData=0
+-insert:ModuleSelection=0
+-insert:IncludeInBuild=2
+-insert:AlwaysBuild=2
+-insert:GenerateAssemblyFile=1
+-insert:AssembleAssemblyFile=1
+-insert:PublicsOnly=2
+-insert:StopOnExitCode=11
+-insert:CustomArgument
+-insert:IncludeLibraryModules
+-insert:BankNo=65535
+-select:..
+-insert:File51
+-selectInserted
+-insert:C51
+-selectInserted
+-insert:RegisterColoring=2
+-insert:VariablesInOrder=2
+-insert:IntegerPromotion=2
+-insert:uAregs=2
+-insert:UseInterruptVector=2
+-insert:Fuzzy=8
+-insert:Optimize=10
+-insert:WarningLevel=3
+-insert:SizeSpeed=2
+-insert:ObjectExtend=2
+-insert:ACallAJmp=2
+-insert:InterruptVectorAddress=0
+-insert:VariousControls
+-selectInserted
+-insert:MiscControls
+-insert:Define
+-insert:Undefine
+-insert:IncludePath
+-select:../../../.."
+	fi
+	libdeps="$libdeps$IFS-select:.."
 	incpaths="$incpaths${incpaths:+;}../${lib%/*}"
 	incfiles="$incfiles${IFS}$lib"
 done
