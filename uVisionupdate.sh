10b5130921f12574efa449cf036c5c2b278a65e4
Dominic Fandrey <kamikaze@bsdforen.de>
2012-10-14 09:57:39 +0200

    Work around ARM issue 531308, by setting simulator.ini in uvopt
    changed uVisionupdate.sh
    - Update the .uvopt file with the simulator.ini location, because ?Vision
      ignores the entry in the .uvproj file

--- uVisionupdate.sh
+++ uVisionupdate.sh
@@ -137,6 +137,16 @@ overlays="$(awk -f ${LIBPROJDIR}/scripts/overlays.awk $incfiles $(find src/ -nam
 echo "$overlays" | sed -e 's/^/	/' -e 's/[[:cntrl:]]$//' 1>&2
 
 echo "Updating uVision/hsk_dev.uvproj ..." 1>&2
+# This is a bug workaround see ARM case 531308
+cp uVision/hsk_dev.uvopt uVision/hsk_dev.uvopt.bak
+awk -f ${LIBPROJDIR}/scripts/xml.awk uVision/hsk_dev.uvopt.bak \
+	-search:DebugOpt/sIfile \
+	-set:"..\\$SIMDIR" \
+	-select:/ \
+	-print > uVision/hsk_dev.uvopt \
+		&& rm uVision/hsk_dev.uvopt.bak \
+		|| mv uVision/hsk_dev.uvopt.bak uVision/hsk_dev.uvopt
+
 cp uVision/hsk_dev.uvproj uVision/hsk_dev.uvproj.bak
 awk -f ${LIBPROJDIR}/scripts/xml.awk uVision/hsk_dev.uvproj.bak \
 	-search:TargetName \
